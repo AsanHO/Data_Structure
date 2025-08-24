7407bb118276db3588cb266273d090fc8e71c03b
AsanHO <eunho@ieunhoui-iMac.local>
2025-08-24 15:32:11 +0900

    cmt : Heap

--- shared/MaxHeap.h
+++ shared/MaxHeap.h
@@ -1,122 +1,112 @@
 ﻿#pragma once
 
 #include <cassert>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
-template<typename T>
-class MaxHeap
-{
-public:
-	MaxHeap(int cap = 10)
-	{
-		assert(cap > 0);
+template <typename T>
+class MaxHeap {
+   private:
+    T* heap_ = nullptr;
+    int size_ = 0;
+    int capacity_ = 0;
 
-		capacity_ = cap;
-		size_ = 0;
-		heap_ = new T[capacity_ + 1]; // heap[0]은 비워둡니다.
-	}
+   public:
+    MaxHeap(int cap = 10) {
+        assert(cap > 0);
 
-	void Resize(int new_capacity)
-	{
-		T* new_heap = new T[new_capacity];
-		memcpy(new_heap, heap_, sizeof(T) * (size_ + 1)); // +1 주의
-		if (heap_) delete[] heap_;
-		heap_ = new_heap;
-		capacity_ = new_capacity;
-	}
+        capacity_ = cap;
+        size_ = 0;
+        heap_ = new T[capacity_ + 1];  // heap[0]은 비워둡니다.
+    }
 
-	T Top()
-	{
-		return heap_[1]; // 1번 인덱스 자리
-	}
+    void Resize(int new_capacity) {
+        T* new_heap = new T[new_capacity];
+        memcpy(new_heap, heap_, sizeof(T) * (size_ + 1));  // +1 주의
+        if (heap_) delete[] heap_;
+        heap_ = new_heap;
+        capacity_ = new_capacity;
+    }
 
-	bool IsEmpty()
-	{
-		return size_ == 0;
-	}
+    T Top() {
+        return heap_[1];  // 1번 인덱스 자리
+    }
 
-	void Print()
-	{
-		using namespace std;
-		cout << "Index: ";
-		for (int i = 1; i <= size_; i++)
-			cout << setw(3) << i;
-		cout << endl;
+    bool IsEmpty() { return size_ == 0; }
 
-		cout << "Value: ";
-		for (int i = 1; i <= size_; i++)
-			cout << setw(3) << heap_[i];
-		cout << endl << endl;
-	}
+    void Print() {
+        using namespace std;
+        cout << "Index: ";
+        for (int i = 1; i <= size_; i++) cout << setw(3) << i;
+        cout << endl;
 
-	void Push(const T& item)
-	{
-		//using namespace std;
-		//cout << "Push " << item << endl;
+        cout << "Value: ";
+        for (int i = 1; i <= size_; i++) cout << setw(3) << heap_[i];
+        cout << endl << endl;
+    }
 
-		if (size_ == capacity_)
-			Resize(capacity_ * 2);
+    void Push(const T& item) {
+        using namespace std;
+        cout << "Push " << item << endl;
 
-		// 삽입: 일단 맨 마지막에 삽입한 후에 부모 노드로 올린다.
+        if (size_ == capacity_) Resize(capacity_ * 2);
 
-		size_ += 1;
-		int current = size_; // 마지막에 추가가될 위치 (인덱스)
+        // 삽입: 일단 맨 마지막에 삽입한 후에 부모 노드로 올린다.
 
-		while (current != 1 /* && TODO */) // 부모 위치의 값이 추가하려는 값보다 작다면
-		{
-			// 부모 위치의 값을 자식 위치로 복사해서 내린다.
-			// TODO:
+        size_ += 1;
+        int current = size_;  // 마지막에 추가가될 위치 (인덱스)
 
-			//cout << "Current = " << current << endl;
-			//Print();
+        while (current != 1 &&
+               item >= heap_[current /
+                             2])  // 부모 위치의 값이 추가하려는 값보다 작다면
+        {
+            // 부모 위치의 값을 자식 위치로 복사해서 내린다.
+            // TODO:
+            cout << "Current = " << current << endl;
+            heap_[current] = heap_[current / 2];
+            Print();
+            // TODO:
+            current = current / 2;
+        }
 
-			// TODO:
-		}
+        heap_[current] = item;  // 최종적으로 결정된 위치에 복사
+    }
 
-		heap_[current] = item; // 최종적으로 결정된 위치에 복사
-	}
+    void Pop() {
+        assert(!IsEmpty());
 
-	void Pop()
-	{
-		assert(!IsEmpty());
+        using namespace std;
+        cout << "Pop()" << endl;
 
-		//using namespace std;
-		//cout << "Pop()" << endl;
+        heap_[1].~T();  // 소멸자 호출
 
-		// heap[1].~T(); // 소멸자 호출 
+        // 삭제: 가장 마지막 값을 루트로 옮긴 후에 내려 보낸다.
 
-		// 삭제: 가장 마지막 값을 루트로 옮긴 후에 내려 보낸다.
+        T last_item = heap_[size_];  // 마지막 아이템 복사
+        size_--;                     // 크기 줄이기
 
-		T last_item = heap_[size_]; // 마지막 아이템 복사
-		size_--;					// 크기 줄이기
-
-		int current = 1;			// 루트 노드에서 시작
-		int child = 2;				// current * 2 (루트의 왼쪽 자식 인덱스)
-		while (child <= size_)
-		{
-			// left, right 중에서 더 큰 자식의 인덱스를 찾는다. 이때 자식이 하나라면 찾을 필요 없음
-			// TODO:
-
-			// 마지막 값이 더 큰 자식의 값 이상이면 더이상 적절한 위치를 찾을 필요가 없기 때문에 루프 중단
-			// TODO:
-
-			// 자식 값을 부모 위치로 복사, 
-			// TODO:
-
-			//cout << "Current = " << current << ", child = " << child << endl;
-			//Print();
-
-			// 그 자식 위치로 current 인덱스 변경, child 인덱스도 그 다음 자식 위치로 변경
-			// TODO:
-		}
-
-		heap_[current] = last_item;
-	}
-
-private:
-	T* heap_ = nullptr;
-	int size_ = 0;
-	int capacity_ = 0;
+        int current = 1;  // 루트 노드에서 시작
+        int child = 2;    // current * 2 (루트의 왼쪽 자식 인덱스)
+        while (child <= size_) {
+            // left, right 중에서 더 큰 자식의 인덱스를 찾는다. 이때 자식이
+            // 하나라면 찾을 필요 없음
+            // TODO:
+            child = heap_[child] >= heap_[child + 1] ? child : child + 1;
+            // 마지막 값이 더 큰 자식의 값 이상이면 더이상 적절한 위치를 찾을
+            // 필요가 없기 때문에 루프 중단
+            // TODO:
+            if (heap_[child] <= last_item) break;
+            // 자식 값을 부모 위치로 복사,
+            // TODO:
+            heap_[current] = heap_[child];
+            cout << "Current = " << current << ", child = " << child << endl;
+            Print();
+            // 그 자식 위치로 current 인덱스 변경, child 인덱스도 그 다음 자식
+            // 위치로 변경
+            // TODO:
+            current = child;
+            child = child * 2;
+        }
+        heap_[current] = last_item;
+    }
 };
-
