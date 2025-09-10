092747536d83eb6b7ab388b4d03f6d2af593f00a
AsanHO <gorma00@naver.com>
2025-09-10 16:12:58 +0900

    cmt : AVL

--- 10.BST/2_AVL/2_AVL.cpp
+++ 10.BST/2_AVL/2_AVL.cpp
@@ -9,9 +9,10 @@ int main() {
 
     AVL<int, char> bst;
 
-    for (int i : {9, 8, 7, 6, 5, 4, 3, 2, 1})
-    // for (int i : { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
-    {
+    // for (int i : {5, 3, 4})
+    // for (int i : {5, 7, 6})
+    // for (int i : {9, 8, 7, 6, 5, 4, 3, 2, 1})
+    for (int i : {1, 2, 3, 4, 5, 6, 7, 8, 9}) {
         bst.Insert(Item{i, char('A' + i)});
         bst.Print2D();
     }
--- shared/AVL.h
+++ shared/AVL.h
@@ -2,157 +2,162 @@
 
 #include "BinarySearchTree.h"
 
-template<typename K, typename V>
-class AVL : public BinarySearchTree<K, V>
-{
-public:
-	using Base = BinarySearchTree<K, V>;
-	using Item = BinarySearchTree<K, V>::Item;
-	using Node = BinarySearchTree<K, V>::Node;
+template <typename K, typename V>
+class AVL : public BinarySearchTree<K, V> {
+   public:
+    using Base = BinarySearchTree<K, V>;
+    using typename BinarySearchTree<K, V>::Item;
+    using typename BinarySearchTree<K, V>::Node;
 
-	//struct Item {
-	//	K key = K();	// first
-	//	V value = V();	// second
-	//};
+    // struct Item {
+    //	K key = K();	// first
+    //	V value = V();	// second
+    // };
 
-	//struct Node {
-	//	Item item;
-	//	Node* left = nullptr;
-	//	Node* right = nullptr;
-	//};
+    // struct Node {
+    //	Item item;
+    //	Node* left = nullptr;
+    //	Node* right = nullptr;
+    // };
 
-	int Height(Node* node)
-	{
-		return Base::Height(node); // 헷갈림 방지용
-	}
+    int Height(Node* node) {
+        return Base::Height(node);  // 헷갈림 방지용
+    }
 
-	int Balance(Node* node)
-	{
-		if (node)
-			return Base::Height(node->left) - Base::Height(node->right);
-		else
-			return 0;
-	}
+    int Balance(Node* node) {
+        if (node)
+            return Base::Height(node->left) - Base::Height(node->right);
+        else
+            return 0;
+    }
 
-	Node* RotateLeft(Node* parent)
-	{
-		// TODO:
+    Node* RotateLeft(Node* parent) {
+        // TODO:
+        Node* child = parent->right;
+        parent->right = child->left;
+        child->left = parent;
+        return child;
+    }
 
-		return nullptr;
-	}
+    Node* RotateRight(Node* parent) {
+        // TODO:
+        //  parent의 왼쪽 자식(left)이 새 루트가 된다.
+        //  left의 오른쪽 서브트리는 parent의 왼쪽 서브트리로 옮겨 붙는다.
+        //  마지막으로 parent를 left의 오른쪽에 연결한다.
+        Node* child = parent->left;
+        parent->left = child->right;
+        child->right = parent;
+        return child;
+    }
 
-	Node* RotateRight(Node* parent)
-	{
-		// TODO:
+    void Insert(const Item& item) { root_ = Insert(root_, item); }
 
-		return nullptr;
-	}
+    Node* Insert(Node* node, const Item& item) {
+        if (!node) return new Node{item, nullptr, nullptr};
 
-	void Insert(const Item& item)
-	{
-		root_ = Insert(root_, item);
-	}
+        const auto& key = item.key;
 
-	Node* Insert(Node* node, const Item& item)
-	{
-		if (!node)
-			return new Node{ item, nullptr, nullptr };
+        if (key < node->item.key)
+            node->left = Insert(node->left, item);
+        else if (key > node->item.key)
+            node->right = Insert(node->right, item);
+        else {
+            node->item = item;
+            return node;
+        }
 
-		const auto& key = item.key;
+        int balance = Balance(node);
 
-		if (key < node->item.key)
-			node->left = Insert(node->left, item);
-		else if (key > node->item.key)
-			node->right = Insert(node->right, item);
-		else
-		{
-			node->item = item;
-			return node;
-		}
+        // balance가 [0, 1, -1](절댓값 1이하) 이면 조절할 필요가 없다고 판단
 
-		int balance = Balance(node);
+        // LL -> Right Rotation
+        if (balance > 1 && Balance(node->left) >= 0) node = RotateRight(node);
 
-		// balance가 0, 1, -1 이면 조절할 필요가 없다고 판단
+        // RR -> Left Rotation
+        if (balance < -1 && Balance(node->right) <= 0) node = RotateLeft(node);
+        //	TODO:
 
-		// LL -> Right Rotation
-		//if (balance > 1 && Balance(node->left) >= 0)
-		//	TODO:
+        // LR -> Left-Right Rotation
+        if (balance > 1 && Balance(node->left) < 0) {
+            node->left = RotateLeft(node->left);
+            node = RotateRight(node);
+        }
 
-		// RR -> Left Rotation
-		//if (balance < -1 && Balance(node->right) <= 0)
-		//	TODO:
+        // RL -> Right-Left Rotation
+        if (balance < -1 && Balance(node->right) > 0) {
+            node->right = RotateRight(node->right);
+            node = RotateLeft(node);
+        }
 
-		// LR -> Left-Right Rotation
-		//if (balance > 1 && Balance(node->left) < 0)
-		//{
-		//	TODO:
-		//}
+        return node;
+    }
 
-		// RL -> Right-Left Rotation
-		//if (balance < -1 && Balance(node->right) > 0)
-		//{
-		//	TODO:
-		//}
+    Node* MinKeyNode(Node* node) {
+        assert(node);
+        while (node->left) node = node->left;
+        return node;
+    }
 
-		return node;
-	}
+    void Remove(const K& key) {
+        using namespace std;
+        cout << "Remove " << key << endl;
+        root_ = Remove(root_, key);
+    }
 
-	Node* MinKeyNode(Node* node)
-	{
-		assert(node);
-		while (node->left)
-			node = node->left;
-		return node;
-	}
+    Node* Remove(Node* node, const K& key) {
+        // BST와 동일
+        if (!node) return node;
 
-	void Remove(const K& key)
-	{
-		using namespace std;
-		cout << "Remove " << key << endl;
-		root_ = Remove(root_, key);
-	}
+        if (key < node->item.key)
+            node->left = Remove(node->left, key);
+        else if (key > node->item.key)
+            node->right = Remove(node->right, key);
+        else {
+            if (!node->left) {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            } else if (!node->right) {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
 
-	Node* Remove(Node* node, const K& key)
-	{
-		// BST와 동일
-		if (!node) return node;
+            Node* temp = MinKeyNode(node->right);
+            node->item = temp->item;
+            node->right = Remove(node->right, temp->item.key);
+        }
 
-		if (key < node->item.key)
-			node->left = Remove(node->left, key);
-		else if (key > node->item.key)
-			node->right = Remove(node->right, key);
-		else
-		{
-			if (!node->left)
-			{
-				Node* temp = node->right;
-				delete node;
-				return temp;
-			}
-			else if (!node->right)
-			{
-				Node* temp = node->left;
-				delete node;
-				return temp;
-			}
+        // if (node == NULL)	return node; // 불필요 (입력 node의 자식이 하나이거나 없는 경우에는 위에서 return 하기 때문)
 
-			Node* temp = MinKeyNode(node->right);
-			node->item = temp->item;
-			node->right = Remove(node->right, temp->item.key);
-		}
+        // 균형 잡기
 
-		//if (node == NULL)	return node; // 불필요 (입력 node의 자식이 하나이거나 없는 경우에는 위에서 return 하기 때문)
+        int balance = Balance(node);
 
-		// 균형 잡기
+        // TODO:
+        // LL -> Right Rotation
+        if (balance > 1 && Balance(node->left) >= 0) node = RotateRight(node);
 
-		int balance = Balance(node);
+        // RR -> Left Rotation
+        if (balance < -1 && Balance(node->right) <= 0) node = RotateLeft(node);
+        //	TODO:
 
-		// TODO:
+        // LR -> Left-Right Rotation
+        if (balance > 1 && Balance(node->left) < 0) {
+            node->left = RotateLeft(node->left);
+            node = RotateRight(node);
+        }
 
-		return node;
-	}
+        // RL -> Right-Left Rotation
+        if (balance < -1 && Balance(node->right) > 0) {
+            node->right = RotateRight(node->right);
+            node = RotateLeft(node);
+        }
 
-private:
-	Node*& root_ = BinarySearchTree<K, V>::root_;
-	// this->root_로도 사용 가능
+        return node;
+    }
+
+   private:
+    Node*& root_ = BinarySearchTree<K, V>::root_;
+    // this->root_로도 사용 가능
 };
