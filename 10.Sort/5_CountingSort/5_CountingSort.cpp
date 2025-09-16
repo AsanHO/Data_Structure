8072feeffdd99688915f32fe2ca2008a086d37b9
AsanHO <gorma00@naver.com>
2025-09-16 17:32:59 +0900

    cmt : sort

--- 10.Sort/5_CountingSort/5_CountingSort.cpp
+++ 10.Sort/5_CountingSort/5_CountingSort.cpp
@@ -1,99 +1,90 @@
-﻿#include <iostream>
-#include "../shared/Queue.h"
-#include <iomanip>
+﻿#include <iomanip>
+#include <iostream>
+
+#include "../../shared/Queue.h"
 
 using namespace std;
 
-bool CheckSorted(int* arr, int size)
-{
-	for (int i = 0; i < size - 1; i++)
-	{
-		if (arr[i] > arr[i + 1])
-			return false;
-	}
+bool CheckSorted(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) return false;
+    }
 
-	return true;
+    return true;
 }
 
-void Print(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-		cout << setw(4) << arr[i] << " ";
-	cout << endl;
+void Print(int* arr, int size) {
+    for (int i = 0; i < size; i++) cout << setw(4) << arr[i] << " ";
+    cout << endl;
 }
 
-int GetMax(int arr[], int n)
-{
-	int mx = arr[0];
-	for (int i = 1; i < n; i++)
-		if (arr[i] > mx)
-			mx = arr[i];
-	return mx;
+int GetMax(int arr[], int n) {
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx) mx = arr[i];
+    return mx;
 }
 
-int main()
-{
-	//int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-	int arr[] = { 1, 0, 2, 3, 4, 2, 3, 1, 2 }; // 0이 1개, 1이 2개, 2가 3개, 4가 1개
-	int n = sizeof(arr) / sizeof(arr[0]);
+int main() {
+    // int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+    int arr[] = {1, 0, 2, 3, 4, 2, 3, 1, 2};  // 0이 1개, 1이 2개, 2가 3개, 4가 1개
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-	Print(arr, n);
+    Print(arr, n);
 
-	// Queue<int> queues[10]; // 기본 동기: 큐를 사용하지 말자! 
-	int count[10 + 1];        // 각각의 큐에 몇 개가 저장될 것인지 + 마지막 하나는 가장 큰 위치를 표시용
-	int* temp = new int[n];   // 임시 저장 메모리, 큐10개가 메모리를 따로따로 사용하는 대신에 하나의 큰 메모리 하나만 사용
-	                          // 대신에 큐가 사용할 최대 각각의 크기를 미리 알아야 한다
+    // Queue<int> queues[10]; // 기본 동기: 큐를 사용하지 말자!
+    int count[10 + 1];       // 각각의 큐에 몇 개가 저장될 것인지 + 마지막 하나는 가장 큰 위치를 표시용
+    int* temp = new int[n];  // 임시 저장 메모리, 큐10개가 메모리를 따로따로 사용하는 대신에 하나의 큰 메모리 하나만 사용
+                             // 대신에 큐가 사용할 최대 각각의 크기를 미리 알아야 한다
 
-	int m = GetMax(arr, n); // 가장 큰 자리수를 찾기 위해서
+    int m = GetMax(arr, n);  // 가장 큰 자리수를 찾기 위해서
 
-	for (int exp = 1; m / exp > 0; exp *= 10)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			temp[i] = arr[i]; // 단순 복사
-			arr[i] = -1;      // 디버깅 편의
-		}
+    for (int exp = 1; m / exp > 0; exp *= 10) {
+        for (int i = 0; i < n; i++) {
+            temp[i] = arr[i];  // 단순 복사
+            arr[i] = -1;       // 디버깅 편의
+        }
 
-		for (int i = 0; i < 10 + 1; i++)
-			count[i] = 0; // 초기화
+        for (int i = 0; i < 10 + 1; i++) count[i] = 0;  // 초기화
 
-		for (int i = 0; i < n; i++)
-			// queues[(arr[i] / exp) % 10].Enqueue(arr[i]); // 큐에 저장하지 않고
-			count[temp[i] / exp % 10] += 1;                 // 각각의 큐에 몇 개가 저장될 것인지만 기록
+        for (int i = 0; i < n; i++)
+            // queues[(arr[i] / exp) % 10].Enqueue(arr[i]); // 큐에 저장하지 않고
+            count[temp[i] / exp % 10] += 1;  // 각각의 큐에 몇 개가 저장될 것인지만 기록
 
-		cout << "Count1: ";
-		Print(count, 11);
+        cout << "Count1: ";
+        Print(count, 11);
 
-		// 큐 10개가 메모리를 따로따로 갖지 않고 한 줄로 쭉 나열되어 있다면 각각의 큐의 시작 위치는?
-		for (int i = 1; i < 10 + 1; i++)
-		{
-			// TODO: 이 부분을 어렵게 생각하시는 분들이 많네요. 
-			//       Count1: 과 Count2: 의 출력을 보고 맞춰보세요.
-		}
+        // 큐 10개가 메모리를 따로따로 갖지 않고 한 줄로 쭉 나열되어 있다면 각각의 큐의 시작 위치는?
 
-		cout << "Count2: ";
-		Print(count, 11);
+        for (int i = 1; i < 10 + 1; i++) {
+            // TODO: 이 부분을 어렵게 생각하시는 분들이 많네요.
+            //       Count1: 과 Count2: 의 출력을 보고 맞춰보세요.
+            count[i] = count[i] + count[i - 1];
+        }
 
-		for (int i = n - 1; i >= 0; i--) // Stable sort로 구현하기 위해 역순으로 진행
-		{
-			cout << temp[i] << endl; // 자기 자리를 찾아갈 값
+        cout << "Count2: ";
+        Print(count, 11);
 
-			// TODO: count[temp[i] / exp % 10] 업데이트 (아주 간단해요)
-			arr[count[temp[i] / exp % 10]] = temp[i];
+        for (int i = n - 1; i >= 0; i--)  // Stable sort로 구현하기 위해 역순으로 진행
+        {
+            cout << temp[i] << endl;  // 자기 자리를 찾아갈 값
 
-			cout << " Count: ";
-			Print(count, 11);
+            // TODO: count[temp[i] / exp % 10] 업데이트 (아주 간단해요)
+            arr[count[temp[i] / exp % 10] - 1] = temp[i];
+            count[temp[i]]--;
+            cout << " Count: ";
+            Print(count, 11);
 
-			cout << "Output: ";
-			Print(arr, n);
-		}
+            cout << "Output: ";
+            Print(arr, n);
+        }
 
-		Print(arr, n);
-	}
+        Print(arr, n);
+    }
 
-	delete[] temp;
+    delete[] temp;
 
-	return 0;
+    return 0;
 }
 
 /* 이해를 돕기 위해 실제 예상 출력에 추가한 부분이 있습니다.
